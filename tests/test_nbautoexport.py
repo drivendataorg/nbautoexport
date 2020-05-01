8552c2291fde9710434cf357349a027ae2a927aa
Robert Gibboni <robert@drivendata.org>
2020-05-01 16:11:31 -0700

    Test for correct use of install_sentinel

--- tests/test_nbautoexport.py
+++ tests/test_nbautoexport.py
@@ -1,31 +1,31 @@
 """Tests for `nbautoexport` package."""
 
-import pytest
-
 from click.testing import CliRunner
+import json
 
-from nbautoexport import nbautoexport
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+from nbautoexport.nbautoexport import (
+    install,
+    install_sentinel,
+)
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    help_result = runner.invoke(nbautoexport.install, ["--help"])
+    help_result = runner.invoke(install, ["--help"])
     assert help_result.exit_code == 0
     assert "Exports Jupyter notebooks to various file formats" in help_result.output
+
+
+def test_install_sentinel(tmp_path_factory):
+    directory = tmp_path_factory.mktemp("install_sentinel")
+    export_formats = ["script", "html"]
+    install_sentinel(export_formats, organize_by="extension", directory=directory, overwrite=False)
+    with (directory / ".nbautoexport").open("r") as fp:
+        config = json.load(fp)
+
+    expected_config = {
+        "export_formats": export_formats,
+        "organize_by": "extension",
+    }
+    assert config == expected_config
