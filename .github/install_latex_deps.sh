c5228484c9d2256ebc555c56a06339601218b32e
Jay Qi <jayqi@users.noreply.github.com>
2020-07-10 19:28:56 -0700

    Install deps

---
+++ .github/install_latex_deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+tlmgr install \
+    adjustbox \
+    caption \
+    collectbox \
+    enumitem \
+    environ \
+    eurosym \
+    float \
+    jknapltx \
+    mathpazo \
+    parskip \
+    pgf \
+    rsfs \
+    tcolorbox \
+    titling \
+    trimspaces \
+    ucs \
+    ulem \
+    upquote
