c8f485cf8abd4b5151f0485ec226fcdf818dd84d
Jay Qi <jayqi@users.noreply.github.com>
2020-07-25 18:51:12 -0700

    Add MIT license

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "Framework :: Jupyter",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
