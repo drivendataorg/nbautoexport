2f4e03517ff7824177f1d8fe17ef04d52510e072
Jay Qi <jayqi@users.noreply.github.com>
2021-09-04 12:14:04 -0400

    Update tests for typer 0.4.0 and click 8

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,15 +6,15 @@ from nbautoexport.nbautoexport import app
 from nbautoexport import __version__
 
 
-def test_main():
-    """Test the CLI main callback."""
+def test_no_command():
+    """Test the CLI errors with no command."""
     runner = CliRunner()
     result = runner.invoke(app)
-    assert result.exit_code == 0
-    assert "Automatically export Jupyter notebooks to various file formats" in result.output
+    assert result.exit_code > 0
+    assert "Error: Missing command." in result.output
 
 
-def test_main_help():
+def test_help():
     """Test the CLI main callback with --help flag."""
     runner = CliRunner()
     result = runner.invoke(app, ["--help"])
@@ -30,17 +30,18 @@ def test_version():
     assert result.output.strip() == __version__
 
 
-def test_main_python_m():
+def test_no_command_python_m():
+    """Test the CLI with python -m errors with no command."""
     result = subprocess.run(
         ["python", "-m", "nbautoexport"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
     )
-    assert result.returncode == 0
-    assert "Automatically export Jupyter notebooks to various file formats" in result.stdout
-    assert result.stdout.startswith("Usage: python -m nbautoexport")
-    assert "Usage: __main__.py" not in result.stdout
+    assert result.returncode > 0
+    assert "Error: Missing command." in result.stderr
+    assert result.stderr.startswith("Usage: python -m nbautoexport")
+    assert "Usage: __main__.py" not in result.stderr
 
 
 def test_version_python_m():
