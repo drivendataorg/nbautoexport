e3ee9c66d26ddd47be1cb357543c3ee289811958
Jay Qi <jayqi@users.noreply.github.com>
2022-11-24 16:24:21 -0500

    Increase verbosity

--- integration_tests/test.sh
+++ integration_tests/test.sh
@@ -2,10 +2,10 @@ jupyter $1 --no-browser&> output.log &
 APP_PID=$!
 sleep 10
 kill $APP_PID
+cat output.log
 if grep -q "nbautoexport | Successfully registered post-save hook." output.log; then
   echo "nbautoexport post-save hook successfully registered"
 else
   echo "nbautoexport post-save hook failed to register"
-  cat output.log
   exit 1
 fi
